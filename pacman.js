8a309c41897765515789b717b44df444b9ad4a28
Don Cooper <don_cooper@hotmail.com>
2017-07-03 10:26:17 -0400

    Power pellets defined and added to display

--- pacman.js
+++ pacman.js
@@ -1,6 +1,7 @@
 // Setup initial game stats
 var score = 0;
 var lives = 2;
+var powerPellets = 4;
 
 // Define your ghosts here
 
@@ -57,6 +58,7 @@ function clearScreen() {
 
 function displayStats() {
   console.log('Score: ' + score + '     Lives: ' + lives);
+  console.log('\nPower-Pellets: ' + powerPellets);
 }
 
 function displayMenu() {
